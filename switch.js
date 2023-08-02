3840c161577b9a1fe4158a21639f3168d181f086
iqra615 <67075928+iqra615@users.noreply.github.com>
2023-08-02 18:43:09 +0200

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,55 @@
+var month= 5;
+switch(month)
+{
+   case 1:
+    document.write("Jaunary");
+    break;
+
+    case 2:
+        document.write("February");
+        break;
+
+        case 3:
+        document.write("March");
+        break;
+
+        case 4:
+        document.write("April");
+        break;
+
+        case 5:
+        document.write("May");
+        break;
+
+        case 6:
+        document.write("Jun");
+        break;
+
+        case 7:
+        document.write("July");
+        break;
+
+        case 8:
+        document.write("August");
+        break;
+
+        case 9:
+        document.write("September");
+        break;
+
+        case 10:
+        document.write("October");
+        break;
+
+        case 11:
+        document.write("November");
+        break;
+
+        case 12:
+        document.write("December");
+        break;
+
+        default:
+            document.write("wrong input");
+
+}
